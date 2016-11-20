60856be65a3fac6ae1b9faf06589e9da95c0c9ba
Theresa Luu <theresa.luu@gmail.com>
2016-11-20 14:36:14 -0800

    added rspec

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,14 @@ module HomeLibrary
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        request_specs: false,
+        controller_specs: true
+    end
   end
 end
