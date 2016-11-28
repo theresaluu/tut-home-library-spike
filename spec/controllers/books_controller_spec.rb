404926ca905b89699ff9afa21ca75c722fe2ef1a
Theresa Luu <theresa.luu@gmail.com>
2016-11-27 21:32:37 -0800

    started controller specs

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe BooksController, type: :controller do
   # Book. As you add validations to Book, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    FactoryGirl.build(:book).attributes
   }
 
   let(:invalid_attributes) {
